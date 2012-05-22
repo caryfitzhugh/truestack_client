a1f27e42277e185433712a51534802825e75ebfc
Cary FitzHugh <cary.fitzhugh@ziplist.com>
2012-05-22 09:57:20 -0400

    Generate config file.

--- lib/truestack_client/configure.rb
+++ lib/truestack_client/configure.rb
@@ -2,6 +2,20 @@ module TruestackClient
   class Configure
     attr_accessor :key, :host, :logger
 
+    CONFIG_OPTIONS = {
+      :host => "http://director.truestack.com",
+      :key  => "ENTER_KEY",
+      :browser_tracking => true,
+      :code_paths => nil,
+      :environments => :production,
+      :logger_path  => 'log/truestack.log'
+    }
+
+    def example_config_file(opts={})
+      opts = CONFIG_OPTIONS.merge(opts)
+      opts.to_yaml
+    end
+
     def logger
       if !@logger
         @logger = Logger.new(STDOUT)
