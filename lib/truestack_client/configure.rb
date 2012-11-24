2e4b36649eb37f74d3051aaf6b44fd9187b4f10e
Cary FitzHugh <cary.fitzhugh@ziplist.com>
2012-11-23 23:50:37 -0500

    update.

--- lib/truestack_client/configure.rb
+++ lib/truestack_client/configure.rb
@@ -52,6 +52,9 @@ module TruestackClient
         else
           @resource_uri = ENV['TRUESTACK_URI']
         end
+        if @(resource_uri == nil || @resource_uri == '')
+          raise "Need to set env['TRUESTACK_URI'] or config/truestack_uri"
+        end
         @resource_uri = @resource_uri.strip
       end
       @resource_uri
